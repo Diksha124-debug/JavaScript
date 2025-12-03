1b1cf69b188b435110d5cb8c60326e448f7bbd49
Diksha Pandey <dp729080@gmail.com>
2025-12-03 06:50:32 +0000

    Conversion in JS

---
+++ basic/p4.js
@@ -0,0 +1,20 @@
+let Score="90abc";
+Score =Number(Score)
+//console.log(typeof Score);
+//console.log(Score);
+//"34"=34
+//"34nan"=NAN
+let num;
+num =String(num)
+//console.log(num)
+//console.log(typeof num)
+//num=""
+//null="null"
+//undefined="undefined"
+let val=undefined;
+val =Boolean(val);
+console.log(typeof val);
+console.log(val)
+//undefined=false
+//""=false
+//23=true
