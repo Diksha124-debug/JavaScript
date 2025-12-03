85dad6e51a74aa2e983432b9c4f5cb621e4161fc
Diksha Pandey <dp729080@gmail.com>
2025-12-03 06:20:58 +0000

    dataTypes

---
+++ basic/p2.js
@@ -0,0 +1,4 @@
+let accountHolder="Diksha Pandey"
+const accountNumber=32564785790
+IFSCcode="SBIN0009939"
+console.table([accountHolder,accountNumber,IFSCcode])
---
+++ basic/p3.js
@@ -0,0 +1,14 @@
+"use strict";  //it will treat js code as newer js version
+/* bollean=true/false
+Number=89\89.45
+string="diksha" '236218' `fdsfi`
+null= type is object 
+undefined =when we have not declared any value
+object=key/value pair
+symbol =ensure uniqueness
+bigint 
+*/
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 123);
+
