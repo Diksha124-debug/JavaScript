b6eec3709abaad65fd0dbcc0f6ce4b230f7e1482
Diksha Pandey <dp729080@gmail.com>
2025-12-04 07:25:50 +0000

    ArrayBasics

--- basic/p10.js
+++ basic/p10.js
@@ -7,9 +7,20 @@ arr.push("ravindra nath ");
 // console.log(arr.pop()) remove last element
 //console.log(arr)
 //console.log(arr.concat(a))
-console.log(arr)
+//console.log(arr)
 //arr.reverse()
-//arr.shift()  shift first array element
+//arr.shift()  shift first array element and shift entire array
 //arr.unshift("naive",90) //add in first and shift index
-arr.push("rraamm")
-console.log(arr)//add at end
+//arr.push("rraamm")
+// console.log(arr)//add at end
+// console.log(arr.length);
+// console.log(arr.indexOf(0));
+// console.log(arr.includes(2))
+//console.log(typeof(arr));
+//console.log(typeof (arr.join("--"))) //string
+//console.log(arr.slice(2)); //to extract 2nd index to last and does not modify original array
+//console.log(arr.slice(2,4));//2nd index to 3rd
+//console.log(arr)
+console.log(arr)
+arr.splice(2,1,"raam",78,40) //modify original array (start_index,delete_count,items_insert_at_start_index)
+console.log(arr)
