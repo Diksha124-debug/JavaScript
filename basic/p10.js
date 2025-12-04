a96a406ce1c31b2fc765a0a26f2ba80bb46fd4ce
Diksha Pandey <dp729080@gmail.com>
2025-12-04 06:54:25 +0000

    arrays

---
+++ basic/p10.js
@@ -0,0 +1,15 @@
+let arr=[2,"diksha",8.9,4,0]
+//console.log(arr);
+let a= new Array(2,3,4,"kalma","pada","tune");
+// console.log(a);
+arr.push("ravindra nath ");
+// console.log(arr)
+// console.log(arr.pop()) remove last element
+//console.log(arr)
+//console.log(arr.concat(a))
+console.log(arr)
+//arr.reverse()
+//arr.shift()  shift first array element
+//arr.unshift("naive",90) //add in first and shift index
+arr.push("rraamm")
+console.log(arr)//add at end
